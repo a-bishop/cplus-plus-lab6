9b6ca7e72a413b1a8bc29a8198bbda0f1340dd02
Andrew Bishop <andrew.bishop53@gmail.com>
2019-03-04 11:02:26 -0800

    Initial Commit

---
+++ L6Q1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  L6Q1
+//
+//  Created by Andrew on 2019-03-04.
+//  Copyright © 2019 ICS214. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
